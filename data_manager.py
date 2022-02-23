eb977466ff47eb584884f16780d45d016cf88a7d
Judd Baroff <self@juddbaroff.com>
2022-02-23 15:57:33 -0600

    format data into class

--- data_manager.py
+++ data_manager.py
@@ -3,19 +3,19 @@ from json import loads
 from keys import SHEETY_BASE8_PASSWORD
 
 sheety_endpoint = "https://api.sheety.co/adb041be0364f24d148351b1aa8c6fb7/cheapFlights/flightList"
-
 header = {
     "Authorization": SHEETY_BASE8_PASSWORD,
 }
 
-flight_data = get(url=sheety_endpoint, headers=header)
-flight_dict = loads(flight_data.text)
-
-
-for n, val in enumerate(flight_dict["flightList"]):
-    print(flight_dict["flightList"][n]["iataCode"])
-
 
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.flight_data = loads(get(url=sheety_endpoint, headers=header).text)
+
+    def get_iata_codes(self):
+        places = {}
+        for n, val in enumerate(self.flight_data["flightList"]):
+            iata_code = self.flight_data["flightList"][n]["iataCode"]
+            price = self.flight_data["flightList"][n]["lowestPrice"]
+            places[iata_code] = price
+        return places
