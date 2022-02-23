ddd49deaab433fb018e494ef46c18cf342fcae7d
Judd Baroff <self@juddbaroff.com>
2022-02-23 16:00:00 -0600

    create central hub for program functionality

---
+++ main.py
@@ -0,0 +1,10 @@
+from data_manager import DataManager
+
+bob = DataManager()
+
+print(bob.get_iata_codes())
+
+#This file will need to use the DataManager,FlightSearch, FlightData,
+#   NotificationManager classes to achieve the program requirements.
+
+
