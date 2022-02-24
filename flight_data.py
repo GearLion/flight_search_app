fd28cca4a37da04983f1cc522f2cf36939e6d11e
Judd Baroff <self@juddbaroff.com>
2022-02-24 15:54:54 -0600

    start organization for flight data

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,20 @@
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+
+
+    def organize_flights(self, list):
+        message_list = []
+        flight_num = 1
+        for flight in list:
+            message = f"Flight: #{flight_num}\n " \
+                      f"Price: {flight['price']}" \
+                      f"Departure: "
+
+            flight_num += 1
+
+# Price,
+# Date and time leaving,
+# Date and time returning,
+# Length of flights,
+# To where,
+# With which airline,
+# .
