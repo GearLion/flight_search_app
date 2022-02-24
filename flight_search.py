1aed0f480569075e7265b8950fed6f15dba76b25
Judd Baroff <self@juddbaroff.com>
2022-02-24 15:55:16 -0600

    update flight search class

--- flight_search.py
+++ flight_search.py
@@ -9,26 +9,29 @@ TEQUILA_HEADER = {
 }
 
 
-class FlightSearch():
+class FlightSearch:
     def __init__(self):
         self.start_date = datetime.now() + timedelta(days=7)
         self.end_date = self.start_date + timedelta(days=187)
 
     def find_flights(self, dictionary):
+        flight_list = []
         for n, (key, val) in enumerate(dictionary.items()):
             params = {
                 "fly_from": "city:DAL",
-                "fly_to": key,  # This needs to populate from data_manager
+                "fly_to": key,
                 "date_from": self.start_date.strftime("%d/%m/%Y"),
                 "date_to": self.end_date.strftime("%d/%m/%Y"),
                 "flight_type": "round",
                 "nights_in_dst_from": 7,
                 "nights_in_dst_to": 28,
                 "curr": "USD",
-                "price_to": val,  # This needs to populate from data_manager
+                "price_to": val,
                 "max_fly_duration": 34,
                 "one_for_city": 1,
             }
 
             flights_json = loads(get(url=TEQUILA_ENDPOINT, headers=TEQUILA_HEADER, params=params).text)
-            print(flights_json)
+            if flights_json["data"]:
+                flight_list.append(flights_json)
+        return flight_list
