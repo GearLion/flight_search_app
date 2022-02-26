4573986061256358efe5e4b57138e7ed8b7385ee
Judd Baroff <self@juddbaroff.com>
2022-02-25 18:40:52 -0600

    finish organization of data for message

--- flight_data.py
+++ flight_data.py
@@ -1,33 +1,49 @@
+from datetime import datetime
+
+
 class FlightData:
 
-    def organize_flights(self, imported_list):
+    def organize_flights(self, flight_list):
         message_list = []
         flight_num = 1
 
-        for flight in imported_list:
-            stopovers_dict = {}
-            price = flight["data"]["price"]
-            for place in flight['data'][0]['route']:
+        for flight in flight_list:
+            # Organize Info for Each Leg
+            flights_text = ""
+            for leg, place in enumerate(flight['data'][0]['route']):
                 origin = place['cityFrom']
                 destination = place["cityTo"]
-                time_departure_local = place["local_departure"] # Clean this up to the time alone -- split along 'T'
-                # I need to get time in air, which should be UTC_Arrival - UTC_Depart.
+                time_departure_local = place["local_departure"].split("T")[1].split(".")[0]
+                departure_utc = place["utc_departure"].split(".")[0]
+                if leg == 0:
+                    first_departure = departure_utc
+                time_arrival_local = place["local_arrival"].split("T")[1].split(".")[0]
+                arrival_utc = place["utc_arrival"].split(".")[0]
+                if leg == len(flight['data'][0]['route']):
+                    final_arrival = arrival_utc
+                time_in_air = datetime.strptime(arrival_utc, "%Y-%m-%dT%H:%M:%S") - datetime.\
+                    strptime(departure_utc, "%Y-%m-%dT%H:%M:%S")
+                this_flight = f"  Leg #{leg + 1}:\n" \
+                              f"    From {origin} to {destination}\n" \
+                              f"    Departure Time: {time_departure_local}\n" \
+                              f"    Arrival Time:   {time_arrival_local}\n" \
+                              f"    Time in Air:    {time_in_air}\n"
+                flights_text += this_flight
 
-
+            # Add It to Info About Whole Trip
             final_destination = flight['data'][0]['route'][0]
-            home = ['cityFrom']
-            for n in len(flight['route']):
+            price = flight["data"]["price"]
+            home = flight['cityFrom']
+            total_time = datetime.strptime(final_arrival, "%Y-%m-%dT%H:%M:%S") - datetime\
+                .strptime(first_departure, "%Y-%m-%dT%H:%M:%S")
 
-            message = f"Flight: #{flight_num}. Price: ${price}" \
-                      f"Route to {final_destination}: " \
-                      f"{flight['route'][0]} "
-
+            message = f"Flight: #{flight_num},\n" \
+                      f"From: {home}\n" \
+                      f"Route to {final_destination}\n" \
+                      f"Price: ${price}\n" \
+                      f"Total Time: {total_time}\n"
+            message += flights_text
             flight_num += 1
+            message_list.append(message)
 
-# Price,
-# Date and time leaving,
-# Date and time returning,
-# Length of flights,
-# To where,
-# With which airline,
-# .
+        return message_list
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 bob = DataManager()
 charlie = FlightSearch()
+tommy = FlightData
 
 dictionary = bob.get_iata_codes()
-sample_dict = (charlie.find_flights(dictionary))
-print(sample_dict[0]['data'][0]['route'][0])
-print("\n This is a buffer \n")
-print(sample_dict[0]['data'][0]['route'][1])
-
+sample_list = (charlie.find_flights(dictionary))
+print(tommy.organize_flights(sample_list))
 
 #This file will need to use the DataManager,FlightSearch, FlightData,
 #   NotificationManager classes to achieve the program requirements.
