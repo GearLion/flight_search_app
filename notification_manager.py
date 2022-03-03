fba2ebebaeaa7e409aa07ef822b3ed654318566a
Judd Baroff <self@juddbaroff.com>
2022-03-03 16:09:43 -0600

    create and send a text based on flight data list

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,14 @@
+from keys import TWILIO_KEY, TWILIO_TOKEN, TWILIO_ORIGIN_NUMBER, TWILIO_DESTINATION_NUMBER
+from twilio.rest import Client
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def send_message(self, message_list):
+        for text in message_list:
+            client = Client(TWILIO_KEY, TWILIO_TOKEN)
+            client.messages \
+                .create(
+                body=f"{text}",
+                from_= TWILIO_ORIGIN_NUMBER,
+                to= TWILIO_DESTINATION_NUMBER
+            )
