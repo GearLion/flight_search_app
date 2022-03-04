33890a0a4600d84edd0bdc4d50619d9368310c3b
Judd Baroff <self@juddbaroff.com>
2022-03-04 15:48:24 -0600

    edit formatting

--- data_manager.py
+++ data_manager.py
@@ -2,15 +2,14 @@ from requests import get
 from json import loads
 from keys import SHEETY_BASE8_PASSWORD
 
-sheety_endpoint = "https://api.sheety.co/adb041be0364f24d148351b1aa8c6fb7/cheapFlights/flightList"
-header = {
-    "Authorization": SHEETY_BASE8_PASSWORD,
-}
-
 
 class DataManager:
     def __init__(self):
-        self.flight_data = loads(get(url=sheety_endpoint, headers=header).text)
+        self.flightlist_endpoint = "https://api.sheety.co/adb041be0364f24d148351b1aa8c6fb7/cheapFlights/flightList"
+        self.header = {
+            "Authorization": SHEETY_BASE8_PASSWORD,
+}
+        self.flight_data = loads(get(url=self.flightlist_endpoint, headers=self.header).text)
 
     def get_iata_codes(self):
         places = {}
--- notification_manager.py
+++ notification_manager.py
@@ -1,14 +1,17 @@
 from keys import TWILIO_KEY, TWILIO_TOKEN, TWILIO_ORIGIN_NUMBER, TWILIO_DESTINATION_NUMBER
 from twilio.rest import Client
 
+
 class NotificationManager:
+    def __init__(self):
+        self.client = Client(TWILIO_KEY, TWILIO_TOKEN)
 
     def send_message(self, message_list):
         for text in message_list:
-            client = Client(TWILIO_KEY, TWILIO_TOKEN)
+            client = self.client
             client.messages \
                 .create(
-                body=f"{text}",
-                from_= TWILIO_ORIGIN_NUMBER,
-                to= TWILIO_DESTINATION_NUMBER
-            )
+                    body=f"{text}",
+                    from_=TWILIO_ORIGIN_NUMBER,
+                    to=TWILIO_DESTINATION_NUMBER
+                )
